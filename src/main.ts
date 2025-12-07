6046332417aa233ea0a9c681e8970fee0aaa0b58
Diogo Mascarenhas <diogomascarenhas0574@gmail.com>
2025-12-07 19:48:40 -0300

    configuracao do zod e swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
+
+  const config = new DocumentBuilder()
+    .setTitle('Kanban API')
+    .setDescription('The Kanban API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ZodValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
